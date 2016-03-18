0bdab848fbdb096bbbd1087df573a7a8fafd5d10
astur <astur@yandex.ru>
2016-03-18 12:09:36 +0300

    options refactoring

--- index.js
+++ index.js
@@ -3,50 +3,29 @@ var needle = require('needle');
 var async = require('async');
 var log = require('cllc')(module);
 
-function filterOpts(opts){
-    opts = Object.prototype.toString.call(opts).slice(8,-1) === 'Object' ? opts : {};
-    var o = {_: {}};
-    o.concurrency = opts.concurrency || 1;
-    o.delay = opts.delay || 10000;
-    o.errorsFirst = !!(opts.errorsFirst);
-
-    o.proxyArray = ({String: [opts.proxy], Array: opts.proxy})[Object.prototype.toString.call(opts.proxy).slice(8,-1)];
-    o.proxyRandom = !(opts.proxyRandom === false);
-
-    o.agentArray = ({String: [opts.user_agent], Array: opts.user_agent})[Object.prototype.toString.call(opts.user_agent).slice(8,-1)];
-    o.agentRandom = !(opts.agentRandom === false);
-
-    if (opts.open_timeout || opts.timeout) {o._.open_timeout = opts.open_timeout || opts.timeout;}
-    if (opts.read_timeout) {o._.read_timeout = opts.read_timeout;}
-
-    o._.cookies = opts.cookies || {};
-    o._.headers = opts.headers || {};
-    if (opts.connection) {o._.connection = opts.connection;}
-    if (opts.decode_response === false) {o._.decode_response = false;}
-
-    o.init = opts.init || function (needle, log, cb){process.nextTick(function(){cb(null, {}, {})});}
-
-    return o;
-}
 
 module.exports = function(startURL, opts, parse, done){
-    var result = [];
-    var passed = {};
-    if (typeof opts === 'function') {
-        done = parse;
-        parse = opts;
+
+    function filterOpts(opts){
+        if (Object.prototype.toString.call(opts).slice(8,-1) !== 'Object') return {cookies: {}, headers: {}};
+        var o = { cookies: opts.cookies || {}, headers: opts.headers || {} };
+
+        if (opts.open_timeout || opts.timeout) {o.open_timeout = opts.open_timeout || opts.timeout;}
+        if (opts.read_timeout) {o.read_timeout = opts.read_timeout;}
+        if (opts.connection) {o.connection = opts.connection;}
+        if (opts.decode_response === false) {odecode_response = false;}
+
+        return o;
     }
-    opts = filterOpts(opts);
-    log.start('%s results found');
 
     function start(){
-        opts.init(needle, log, function(err, cookies, headers){
+        init(needle, log, function(err, cookies, headers){
             if(err){
                 log.e('Init error:', err.message);
-                setTimeout(start, opts.delay);
+                setTimeout(start, delay);
             } else {
-                for(var key in cookies){opts._.cookies[key] = cookies[key];}
-                for(var key in headers){opts._.headers[key] = headers[key];}
+                for(var key in cookies){opts.cookies[key] = cookies[key];}
+                for(var key in headers){opts.headers[key] = headers[key];}
                 q.resume();
                 log.i('Resumed!', new Date());
             }
@@ -84,17 +63,39 @@ module.exports = function(startURL, opts, parse, done){
         }
     }
 
-    var getProxy = opts.proxyArray && getFromArray(opts.proxyArray);
-    var getAgent = opts.agentArray && getFromArray(opts.agentArray);
+    var result = [];
+    var passed = {};
+
+    if (typeof opts === 'function') {
+        done = parse;
+        parse = opts;
+    }
+
+    var concurrency = opts.concurrency || 1;
+    var delay = opts.delay || 10000;
+    var errorsFirst = !!(opts.errorsFirst);
+
+    var proxyArray = ({String: [opts.proxy], Array: opts.proxy})[Object.prototype.toString.call(opts.proxy).slice(8,-1)];
+    var proxyRandom = !(opts.proxyRandom === false);
+
+    var agentArray = ({String: [opts.user_agent], Array: opts.user_agent})[Object.prototype.toString.call(opts.user_agent).slice(8,-1)];
+    var agentRandom = !(opts.agentRandom === false);
+
+    var init = opts.init || function (needle, log, cb){process.nextTick(function(){cb(null, {}, {})});}
+
+    opts = filterOpts(opts);
+
+    var getProxy = proxyArray && getFromArray(proxyArray);
+    var getAgent = agentArray && getFromArray(agentArray);
 
     var q = async.queue(function(url, cb){
-        if (opts.proxyArray) {
-            opts._.proxy = opts.proxyRandom ? getProxy(true) : opts._.proxy || getProxy();
+        if (proxyArray) {
+            opts.proxy = proxyRandom ? getProxy(true) : opts.proxy || getProxy();
         }
-        if (opts.agentArray) {
-            opts._.user_agent = opts.agentRandom ? getAgent(true) : opts._.user_agent || getAgent();
+        if (agentArray) {
+            opts.user_agent = agentRandom ? getAgent(true) : opts.user_agent || getAgent();
         }
-        needle.get(url, opts._, function(err, res){
+        needle.get(url, opts, function(err, res){
             if (!err && res.statusCode === 200) {
                 var $ = typeof res.body === 'string' ? cheerio.load(res.body) : res.body;
                 parse(url, $, {
@@ -108,15 +109,15 @@ module.exports = function(startURL, opts, parse, done){
                 if (!q.paused) {
                     q.pause();
                     log.w('Paused!', new Date());
-                    if (opts.proxyArray && !opts.proxyRandom) {opts._.proxy = getProxy();}
-                    if (opts.agentArray && !opts.agentRandom) {opts._.user_agent = getAgent();}
-                    setTimeout(start, opts.delay);
+                    if (proxyArray && !proxyRandom) {opts.proxy = getProxy();}
+                    if (agentArray && !agentRandom) {opts.user_agent = getAgent();}
+                    setTimeout(start, delay);
                 }
-                q[opts.errorsFirst ? 'unshift' : 'push'](url);
+                q[errorsFirst ? 'unshift' : 'push'](url);
             }
             cb();
         });
-    }, opts.concurrency);
+    }, concurrency);
 
     q.drain = function(){
         log.finish();
@@ -124,6 +125,8 @@ module.exports = function(startURL, opts, parse, done){
             done(result);
         }
     };
+
+    log.start('%s results found');
 
     q.pause();
     safePush()(startURL);
